2f47a4415eb8a6c400c968ae3522451a15d654e1
kajalusadadiya <kajalvpatel07@gmail.com>
2022-01-11 22:04:10 +0000

    This is HW Week-19 restful-booker

---
+++ src/main/java/com/booker/restful/constants/EndPoints.java
@@ -0,0 +1,9 @@
+package com.booker.restful.constants;
+
+public class EndPoints {
+    public static final String CREATE_BOOKING = "/booking";
+    public static final String GET_SINGLE_BOOKING_BY_ID = "/{bookingID}";
+    public static final String UPDATE_BOOKING_BY_ID = "/{bookingID}";
+    public static final String DELETE_BOOKING_BY_ID = "/{bookingID}";
+    public static final String GET_ALL_BOOKINGS_BY_NAME = "?firstname=";
+}
